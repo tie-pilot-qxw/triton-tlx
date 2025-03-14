e40a179e6659d6dd6170521a605daf0300895bc2
Hongtao Yu <hoy@fb.com>
2025-03-13 17:31:21 -0700

    override

--- python/gemmbench/impls/matmul_persistent_tma_ws.py
+++ python/gemmbench/impls/matmul_persistent_tma_ws.py
@@ -98,7 +98,7 @@ class TmaAutoTuneHelper:
         triton.Config(
             {
                 "BLOCK_SIZE_M": 128,
-                "BLOCK_SIZE_N": 128,
+                "BLOCK_SIZE_N": 256,
                 "BLOCK_SIZE_K": 64,
                 "GROUP_SIZE_M": 8,
             },
