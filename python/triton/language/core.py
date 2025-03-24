74d6b4f99950c37aacad8272d85d1fa8bc2f4fb9
Hongtao Yu <hoy@meta.com>
2025-03-24 09:52:05 -0700

    [WS] Deduplicate tl.async_task (#70)
    Also support specifying task_id with `const_expr` values, such as
    
    ```
    @triton.jit
    def kernel(ptr, CONSUMER_TASK: tl.constexpr):
        with tl.async_task([CONSUMER_TASK]):
                tl.store(...)
    ```
    
    Also deduplcating task ids specified with  tl.async_task, e.g
    
    ```
            with tl.async_task([1, NUM_CONSUMER_GROUPS]):
                tl.store(...)
    ```

--- python/triton/language/core.py
+++ python/triton/language/core.py
@@ -2544,7 +2544,7 @@ class async_task:
     Context manager to run code fragments asynchronously.
     """
     def __init__(self, task_ids, _builder=None):
-        self.task_ids = task_ids
+        self.task_ids = list({_constexpr_to_value(tid) for tid in task_ids})
         self.builder = _builder
 
     def __enter__(self):
