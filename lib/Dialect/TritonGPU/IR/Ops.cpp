aca38d19b57941e2cf3eef069af40aebb5fa6d34
Manman Ren <mren@meta.com>
2025-05-08 10:30:05 -0700

    Port from fb/beta ws support
    Summary:
    Pick from ws: try to make ttgir deterministic (#59) (#84) (#105)
    But with fixes to make it work with beta hash. Cleaned up a bunch of stuff, some may not exist in ws-main.
    Missing:
    [WS] code partitioning refactoring, separate out functions to specialize region (#108)
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- lib/Dialect/TritonGPU/IR/Ops.cpp
+++ lib/Dialect/TritonGPU/IR/Ops.cpp
@@ -159,12 +159,13 @@ struct CanonicalizeConvertFromAlloc
 
   mlir::LogicalResult
   matchAndRewrite(triton::gpu::LocalAllocOp op,
-                  PatternRewriter &rewriter) const override {
+                  PatternRewriter &baseRewriter) const override {
     if (!op.getSrc())
       return failure();
     auto convert = op.getSrc().getDefiningOp<ConvertLayoutOp>();
     if (!convert)
       return failure();
+    PatternRewriterWithAsyncTaskIds rewriter(baseRewriter, op);
     rewriter.replaceOpWithNewOp<triton::gpu::LocalAllocOp>(
         op, op->getResult(0).getType(), convert.getSrc());
     return mlir::success();
@@ -178,10 +179,11 @@ struct CanonicalizeConvertFromLocalStore
 
   mlir::LogicalResult
   matchAndRewrite(triton::gpu::LocalStoreOp op,
-                  PatternRewriter &rewriter) const override {
+                  PatternRewriter &baseRewriter) const override {
     auto convert = op.getSrc().getDefiningOp<ConvertLayoutOp>();
     if (!convert)
       return failure();
+    PatternRewriterWithAsyncTaskIds rewriter(baseRewriter, op);
     rewriter.replaceOpWithNewOp<triton::gpu::LocalStoreOp>(op, convert.getSrc(),
                                                            op.getDst());
     return mlir::success();
@@ -292,7 +294,8 @@ struct CanonicalizeConvertFromConvert
 
     // cvt(cvt(x, type1), type2) -> cvt(x, type2)
     if (auto cvt = dyn_cast<ConvertLayoutOp>(arg)) {
-      rewriter.replaceOpWithNewOp<triton::gpu::ConvertLayoutOp>(
+      PatternRewriterWithAsyncTaskIds rewriterTask(rewriter, cvt);
+      rewriterTask.replaceOpWithNewOp<triton::gpu::ConvertLayoutOp>(
           op, op->getResultTypes().front(), cvt.getSrc());
       return success();
     }
