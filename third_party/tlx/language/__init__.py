1e7aae4e81375a5abce27bccaacd10c93c47e249
Arda <ardau@meta.com>
2025-05-16 12:10:25 -0700

    Revert "[TLX] Add buffer indexing"
    This reverts commit 6b251c7afdc4979957abbe0d7665d31297c4af7d.

--- third_party/tlx/language/__init__.py
+++ third_party/tlx/language/__init__.py
@@ -1,11 +1,10 @@
 from .async_task import async_task, async_tasks
 from .types import buffered_tensor
-from .mem_ops import *
+from .mem_ops import local_alloc
 
 __all__ = [
     "async_task",
     "async_tasks",
     "buffered_tensor",
     "local_alloc",
-    "get_buffer",
 ]
