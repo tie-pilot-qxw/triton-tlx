facebookexperimental/triton

[TLX] Introducing Triton low-level language extension (#109)
TLX (Triton Low-level Language Extensions) is a low-level, warp-aware,
hardware-near extension of the Triton DSL. It provides intrinsics and
warp-specialized operations for fine-grained GPU control,
hardware-oriented primitives for advanced kernel development, and
explicit constructs for GPU memory, compute, and asynchronous control
flow, designed for power users pushing Triton to the metal.

This PR adds scaffolding for TLS and introduces async task related ops
for warp specialization support through explicit async task programming

```
@triton.jit
def add_kernel(x_ptr, y_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
    pid = tl.program_id(axis=0)
    block_start = pid * BLOCK_SIZE
    offsets = block_start + tl.arange(0, BLOCK_SIZE)
    mask = offsets < n_elements

    # allocate NUM_STAGES buffers
    x_buf = tlx.local_alloc(BLOCK_SIZE, 1)
    y_buf = tlx.local_alloc(BLOCK_SIZE, 1)

   # allocate barriers
    bar = tlx.alloc_barrier(1)

    with tlx.async_tasks():
        with tlx.async_task("default"):
            x_buf = tlx.async_load(x_ptr + offsets, mask=mask)
            y_buf = tlx.async_load(y_ptr + offsets, mask=mask)
            tlx.barrier_arrive(bar)
        with tlx.async_task(num_warps = 4):
            tlx.barrier_wait(bar)
            x = tlx.local_load(x_buf)
            y = tlx.local_load(y_buf)
            output = x + y
            tl.store(output_ptr + offsets, output, mask=mask)
```


This PR adds initial codegen support for `tlx.async_tasks` and
`tlx.async_task`, which are translated to `ttg.warp_specialize`
structure.

```
ttg.warp_specialize"() <{partitionNumWarps = array<i32: 4>}> ({
      ttg.warp_yield"() : () -> () loc(#loc14)
    }, {
      "ttg.warp_specialize.partitions"() : () -> () loc(#loc6)
    }) : () -> () loc(#loc6)
```

third_party/tlx/language/async_task.py
@@ -1,0 +1,41 @@
+from triton.language import core
+
+
+class async_task:
+    """
+    Context manager to run code fragments asynchronously.
+    """
+    def __init__(self, *args, _builder=None, **kwargs):
+        self.builder = _builder
+        # Handle the optional positional argument like [0]
+        self.is_default = False
+        self.is_explict = False
+        self.task_ids = None
+        self.num_warps = None
+        if args:
+            assert len(args) == 1
+            if isinstance(args[0], core.constexpr) and args[0] == "default":
+                self.is_explict = True
+                self.is_default = True
+            else:
+                self.task_ids = list({core._constexpr_to_value(tid) for tid in args[0]})
+        else:
+            self.is_explict = True
+            self.num_warps = core._constexpr_to_value(kwargs.get("num_warps", None))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+
+class async_tasks:
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass

tie-pilot-qxw/triton-tlx

third_party/tlx/language/async_task.py
@@ -1,0 +1,41 @@
+from triton.language import core
+
+
+class async_task:
+    """
+    Context manager to run code fragments asynchronously.
+    """
+    def __init__(self, *args, _builder=None, **kwargs):
+        self.builder = _builder
+        # Handle the optional positional argument like [0]
+        self.is_default = False
+        self.is_explict = False
+        self.task_ids = None
+        self.num_warps = None
+        if args:
+            assert len(args) == 1
+            if isinstance(args[0], core.constexpr) and args[0] == "default":
+                self.is_explict = True
+                self.is_default = True
+            else:
+                self.task_ids = list({core._unwrap_if_constexpr(tid) for tid in args[0]})
+        else:
+            self.is_explict = True
+            self.num_warps = core._unwrap_if_constexpr(kwargs.get("num_warps", None))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+
+class async_tasks:
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass