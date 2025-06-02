facebookexperimental/triton

[TLX] introduce tlx.local_alloc (#113)
Introducing `tlx.local_alloc` which can be used to allocate a number of
shared memory buffers.

For example, to allocate `NUM_BUFFERS` number of `float16` buffers, each
has `BLOCK_MxBLOCK_K` shape:

` buffers = tlx.local_alloc((BLOCK_M, BLOCK_K), tl.float16,
NUM_BUFFERS)`

where `BLOCK_M`, `BLOCK_K`, `NUM_BUFFERS` are all of `tl.constexpr`
type.

The op will be translated to the following-like TTGIR op:

` %0 = "ttg.local_alloc"() : () -> !ttg.memdesc<4x128x128xf16, #shared,
#smem>
`

For now users have no control of specifying the swizzling mode for the
buffers. The compiler will automatically figure that out based on how
they are consumed, in a separate PR.

third_party/tlx/language/mem_ops.py
@@ -1,0 +1,23 @@
+import triton.language.core as tl
+
+from . import types as tlx
+
+
+@tl.builtin
+def local_alloc(
+    shape: tuple,
+    dtype: tl.dtype,
+    num: tl.constexpr,
+    _builder=None,
+) -> tlx.buffered_tensor:
+    """
+    Allocates buffer in shared memory and return a view of the buffer.
+    """
+    unwrapped_shape = [tl._constexpr_to_value(dim) for dim in shape]
+    unwrapped_num = tl._constexpr_to_value(num)
+    full_shape = [unwrapped_num] + unwrapped_shape
+    dtype = tl._constexpr_to_value(dtype)
+    elem_type = dtype.to_ir(_builder)
+    return tlx.buffered_tensor(
+        _builder.create_local_alloc(full_shape, elem_type),
+    )

tie-pilot-qxw/triton-tlx

third_party/tlx/language/mem_ops.py
@@ -1,0 +1,23 @@
+import triton.language.core as tl
+
+from . import types as tlx
+
+
+@tl.builtin
+def local_alloc(
+    shape: tuple,
+    dtype: tl.dtype,
+    num: tl.constexpr,
+    _builder=None,
+) -> tlx.buffered_tensor:
+    """
+    Allocates buffer in shared memory and return a view of the buffer.
+    """
+    unwrapped_shape = [tl._unwrap_if_constexpr(dim) for dim in shape]
+    unwrapped_num = tl._unwrap_if_constexpr(num)
+    full_shape = [unwrapped_num] + unwrapped_shape
+    dtype = tl._unwrap_if_constexpr(dtype)
+    elem_type = dtype.to_ir(_builder)
+    return tlx.buffered_tensor(
+        _builder.create_local_alloc(full_shape, elem_type),
+    )