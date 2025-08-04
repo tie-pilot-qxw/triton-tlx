821fc06602ea3fd521ceb73bac14458aaa2e368d
Peng Chen <pchen7e2@gmail.com>
2025-08-04 15:57:26 -0700

    [TLX] fix tensordesc layout propagation (#248)
    With TLX, we have two problems:
    - We might have WS ops that tensordesc layout propagation pass does not
    recognize, because WS ops can be created earlier than that pass now
    - We might have TTNG TMA load/store ops created earlier than that pass
    (previously TMA lowering is later so that pass only needs to recognize
    tt descriptor load/store ops)
    
    This PR fixes it with these changes:
    - When seeding tensor desc layout, support
    `ttng::AsyncTMACopyLocalToGlobalOp` and
    `ttng::AsyncTMACopyGlobalToLocalOp`. Due to the nature of these ops, we
    can get "desired shared mem layouts" from the op itself directly (unlike
    `tt.descriptor_load` that needs to infer that from the users of the
    result distributed tensor).
    - When propagating layouts, always tie args of all regions together.
    e.g. If we want to change layout of arg 2 of a region, we must change
    arg 2 of all regions together. We also make the propagation process
    `WarpSpecializePartitionsOp` when propagating to definitions and process
    `WarpSpecializeOp` when propagating to users.
    
    I added two test cases here:
    - One is propagating from outside to ws ops (the pass also seeds
    fallback layout from args of device function boundaries)
    - One is propagating from a use inside one non default region to outside
    
    `make test-lit` and `pytest -vs python/test/unit/language/test_tlx.py`
    all pass. `python third_party/tlx/tutorials/gemm-WS-blackwell.py` now
    runs through without compilation failures any more.
    Conflicts:
    	lib/Dialect/TritonNvidiaGPU/Transforms/OptimizeDescriptorEncoding.cpp
    	test/TritonNvidiaGPU/optimize_descriptor_encoding.mlir

--- lib/Dialect/TritonNvidiaGPU/Transforms/OptimizeDescriptorEncoding.cpp
+++ lib/Dialect/TritonNvidiaGPU/Transforms/OptimizeDescriptorEncoding.cpp
@@ -15,6 +15,7 @@
 #include <unordered_set>
 
 namespace ttg = mlir::triton::gpu;
+namespace ttng = mlir::triton::nvidia_gpu;
 
 namespace {
 
@@ -91,6 +92,42 @@ std::optional<UseInfo> getUseInfo(Operation *op) {
     info.shape = expandToRank(shape, rank);
     return info;
   }
+  if (auto load = dyn_cast<ttng::AsyncTMACopyGlobalToLocalOp>(op)) {
+    // for now, assume each AsyncTMACopyGlobalToLocalOp is bundled with a
+    // TensorDescToTMAPtrOp
+    auto tensorDescToTMAPtrOp =
+        load.getDescPtr().getDefiningOp<ttng::TensorDescToTMAPtrOp>();
+    if (!tensorDescToTMAPtrOp)
+      llvm::report_fatal_error("AsyncTMACopyGlobalToLocalOp expected to be "
+                               "bundled with a TensorDescToTMAPtrOp");
+    auto desc = tensorDescToTMAPtrOp.getDesc();
+    info.descriptor = desc;
+    info.desiredSharedEncoding = load.getResult().getType().getEncoding();
+    assert(isTMACompatibleEncoding(info.desiredSharedEncoding) &&
+           "expecting TMA compatible encoding");
+    info.ctaLayout = ttg::getCTALayout(info.desiredSharedEncoding);
+    auto shape = load.getResult().getType().getShape();
+    auto rank = desc.getType().getBlockType().getRank();
+    info.shape = expandToRank(shape, rank);
+    return info;
+  }
+  if (auto store = dyn_cast<ttng::AsyncTMACopyLocalToGlobalOp>(op)) {
+    // for now, assume each AsyncTMACopyGlobalToLocalOp is bundled with a
+    // TensorDescToTMAPtrOp
+    auto tensorDescToTMAPtrOp =
+        store.getDescPtr().getDefiningOp<ttng::TensorDescToTMAPtrOp>();
+    if (!tensorDescToTMAPtrOp)
+      llvm::report_fatal_error("AsyncTMACopyGlobalToLocalOp expected to be "
+                               "bundled with a TensorDescToTMAPtrOp");
+    auto desc = tensorDescToTMAPtrOp.getDesc();
+    info.descriptor = desc;
+    auto encoding = store.getSrc().getType().getEncoding();
+    info.ctaLayout = ttg::getCTALayout(encoding);
+    auto shape = store.getSrc().getType().getShape();
+    auto rank = desc.getType().getBlockType().getRank();
+    info.shape = expandToRank(shape, rank);
+    return info;
+  }
   return std::nullopt;
 }
 
@@ -127,6 +164,53 @@ namespace nvidia_gpu {
 #include "triton/Dialect/TritonNvidiaGPU/Transforms/Passes.h.inc"
 
 namespace {
+
+SmallVector<Value> getTiedArgs(Operation *op, int resultIdx) {
+  if (auto forOp = dyn_cast<scf::ForOp>(op)) {
+    auto iterArg = forOp.getRegionIterArg(resultIdx);
+    auto result = forOp.getResult(resultIdx);
+    auto yieldVal = forOp.getBody()->getTerminator()->getOperand(resultIdx);
+    auto initVal = forOp.getInitArgs()[resultIdx];
+    return {iterArg, result, yieldVal, initVal};
+  } else if (auto whileOp = dyn_cast<scf::WhileOp>(op)) {
+    auto iterArg = whileOp.getBeforeArguments()[resultIdx];
+    auto result = whileOp.getResults()[resultIdx];
+    auto yieldVal =
+        whileOp.getBeforeBody()->getTerminator()->getOperand(resultIdx);
+    auto initVal = whileOp.getOperands()[resultIdx];
+    return {iterArg, result, iterArg, initVal};
+  } else if (auto ifOp = dyn_cast<scf::IfOp>(op)) {
+    SmallVector<Value> values;
+    for (auto &block : ifOp.getThenRegion().getBlocks()) {
+      auto terminator = block.getTerminator();
+      if (isa<scf::YieldOp>(terminator))
+        values.push_back(terminator->getOperands()[resultIdx]);
+    }
+    for (auto &block : ifOp.getElseRegion().getBlocks()) {
+      auto terminator = block.getTerminator();
+      if (isa<scf::YieldOp>(terminator))
+        values.push_back(terminator->getOperands()[resultIdx]);
+    }
+    values.push_back(ifOp->getResults()[resultIdx]);
+    return values;
+  } else if (auto warpSpecializeOp = dyn_cast<ttg::WarpSpecializeOp>(op)) {
+    // add arg for every partition including default partition
+    SmallVector<Value> values = {warpSpecializeOp.getOperands()[resultIdx]};
+    for (auto region : warpSpecializeOp.getPartitionRegions()) {
+      auto &firstBlock = region->getBlocks().front();
+      values.push_back(firstBlock.getArguments()[resultIdx]);
+    }
+    return values;
+  } else if (auto warpSpecializePartitionsOp =
+                 dyn_cast<ttg::WarpSpecializePartitionsOp>(op)) {
+    auto warpSpecializeOp = dyn_cast<ttg::WarpSpecializeOp>(
+        warpSpecializePartitionsOp->getParentOp());
+    assert(warpSpecializeOp && "expected WarpSpecializeOp");
+    // delegate to parent op
+    return getTiedArgs(warpSpecializeOp, resultIdx);
+  }
+  return {};
+}
 
 const EncodingInfo *internEncoding(std::unordered_set<EncodingInfo> &encodings,
                                    EncodingInfo info) {
@@ -299,6 +383,9 @@ void assignMemoryLayouts(FuncOp &func) {
       } else if (isa<scf::YieldOp>(op)) {
         auto vals = getTiedArgs(op->getParentOp(), use.getOperandNumber());
         updateEncoding(vals, EncodingInfo{});
+      } else if (isa<ttg::WarpSpecializeOp>(op)) {
+        auto vals = getTiedArgs(op, use.getOperandNumber());
+        updateEncoding(vals, EncodingInfo{});
       }
     }
 
@@ -311,7 +398,8 @@ void assignMemoryLayouts(FuncOp &func) {
       }
     } else if (auto blockArg = dyn_cast<BlockArgument>(desc)) {
       auto parentOp = blockArg.getOwner()->getParentOp();
-      if (isa<scf::ForOp, scf::WhileOp>(parentOp)) {
+      if (isa<scf::ForOp, scf::WhileOp, ttg::WarpSpecializePartitionsOp>(
+              parentOp)) {
         auto offset = isa<scf::ForOp>(parentOp);
         auto vals = getTiedArgs(parentOp, blockArg.getArgNumber() - offset);
         updateEncoding(vals, EncodingInfo{});
